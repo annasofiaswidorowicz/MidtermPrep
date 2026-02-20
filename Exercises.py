d2a08356e9bc04767370c553eb43eec4a1062df8
annasofiaswidorowiczlara <annasofiaswidorowicz@gmail.com>
2026-02-20 11:12:11 +0100

    Q3

--- Exercises.py
+++ Exercises.py
@@ -11,3 +11,42 @@ print([1,2,3].append("John"))
 print("bubu"*2)
 print(len(["abc", 2]))
 print(2+3*2**2)
+
+#Assume operations in order - what will each display
+a = 2
+b = 3
+c = "abc"
+print(a, b, c)
+print(a, b, c, sep=",")
+a += 2
+a == 5
+print(a)
+print(c*(a-b))
+d = c.find("b")
+print(d)
+print(d and b)
+print(d == True)
+e = str(a) + str(b) + str(c) + str(d)
+print(e)
+print(e[1::2])
+print(e+e[::-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
