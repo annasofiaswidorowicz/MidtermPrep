8f4eb0bc78889c5dfc47f9c5fe03013bf379b028
annasofiaswidorowiczlara <annasofiaswidorowicz@gmail.com>
2026-02-20 12:06:35 +0100

    Q4

--- Exercises.py
+++ Exercises.py
@@ -31,9 +31,45 @@ print(e)
 print(e[1::2])
 print(e+e[::-1])
 
+#write a function that takes the name of a text file as parameter.
+#Print out the 3-letter words that start with "b"
 
+import requests
+def download_book(url):
+    """
+    Downloads a book from a url
+    :param url: the url for the book
+    :return: None
+    """
+    response = requests.get(url)
+    print(response.status_code)
+    #save the book
+    with open("book.txt", "w") as f:
+        f.write(response.text)
 
+download_book("https://www.gutenberg.org/ebooks/64317.txt.utf-8")
 
+def find_3_letter_words(book_name):
+    """
+    find 3 letter words starting with B
+    :param book_name: the file containing the book
+    :return: None
+    """
+    unique_words = []
+    special_chars = ",.?!;'"
+    with open(book_name, "r") as f:
+        for line in f:
+            #remove punctuation
+            for c in special_chars:
+                line = line.replace(c, "")
+            #break down the line into words
+            words = line.split(" ")
+            for word in words:
+                if word not in unique_words and len(word) == 3 and word [0] == "b":
+                    unique_words.append(word)
+    print(unique_words)
+
+find_3_letter_words("book.txt")
 
 
 
