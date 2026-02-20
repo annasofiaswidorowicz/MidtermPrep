a28cdc00364fbc8e8662f57772b3464893a9733a
annasofiaswidorowiczlara <annasofiaswidorowicz@gmail.com>
2026-02-20 11:03:57 +0100

    Q2

--- Exercises.py
+++ Exercises.py
@@ -2,3 +2,12 @@
 print(type(2+3))
 print(type("abc".find))
 print(type(print(2*2)))
+
+#Indicate the result
+print(2+3)
+print(6/2)
+print(5 or 6)
+print([1,2,3].append("John"))
+print("bubu"*2)
+print(len(["abc", 2]))
+print(2+3*2**2)
